677e2276e4119d8f161750e2d04ea99a86b5cea6
Micheal Wong <wongkaishen2003@hotmail.com>
2025-01-07 03:39:42 +0800

    update views.py
    update views.py
    signup require
    coordinate or address

--- foodpicker/restaurant/urls.py
+++ foodpicker/restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path("login/", auth_views.LoginView.as_view(template_name="accounts/signin.html"), name="login"),
     path('',views.home,name="res.home"),#redirect to home
     path('about/',views.about,name="res.about"),   #redirect to about page
     path('search/',views.search,name="res.search"),  #redirect to search page
@@ -10,10 +12,7 @@ urlpatterns = [
     path('signin',views.signin,name="signin"),#redirect home after signin or signup
     path('signout',views.signout,name="signout"),#redirect home after signout
     path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'), #redirect to home after verification
-
     path('form',views.location_view,name="res.form"), #redirect to form
-
-
     path('restaurant/',views.get_res_list,name="res.res"), #for the restaurant list
     path('restaurant/<int:id>/', views.get_res_detail, name='restaurant_detail'), #for the restaurant detail id
     path('map/',views.get_res_map,name="res.map"),   #for the whole map view of the restaurant
